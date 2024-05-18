2343df53142f9eda78752a51b5811cae69695051
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-18 08:33:46 +0200

    Initial commit

---
+++ src/main/java/org/ericwubbo/derivedqueriesdemo/DerivedQueriesDemoApplication.java
@@ -0,0 +1,13 @@
+package org.ericwubbo.derivedqueriesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerivedQueriesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerivedQueriesDemoApplication.class, args);
+	}
+
+}
